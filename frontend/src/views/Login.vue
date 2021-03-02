82eaa26c662c81505aaf49de377810d5b4e0459b
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-03-02 22:22:13 +0530

    add login form

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,9 +1,25 @@
 <template>
-  todo
+  <form>
+    <label for="id"></label>
+    <input type="text" name="id" v-model="id" />
+    <button type="submit" @click.stop.prevent="submit()">
+      join/create room
+    </button>
+  </form>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      id: ""
+    };
+  },
+  methods: {
+    submit() {
+      this.$router.push("/room/" + this.id);
+    }
+  }
 };
 </script>
